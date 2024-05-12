20a31f979f32315319c69a1edb231e92db869ddc
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-05-12 16:22:37 +0530

    commit

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import list from "../../public/list.json";
 
 function Cards({ item }) {
   console.log(item);
@@ -7,17 +8,14 @@ function Cards({ item }) {
       <div>
         <div className="card w-96 bg-base-100 shadow-xl">
           <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
+            <img src={item.image} alt="Shoes" />
           </figure>
           <div className="card-body">
             <h2 className="card-title">
-              {item.title}
-              <div className="badge badge-secondary">NEW</div>
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
             </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <p>{item.title}</p>
             <div className="card-actions justify-end">
               <div className="badge badge-outline">Fashion</div>
               <div className="badge badge-outline">Products</div>
