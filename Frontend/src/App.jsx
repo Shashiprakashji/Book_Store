fbb8c9fd95bc394a04f7440e643ee0ace6ae22e4
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-05-12 10:46:58 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Freebook from "./components/Freebook";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Freebook></Freebook>
       <Footer />
     </>
   );
--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,7 +1,93 @@
 import React from "react";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
+
+import list from "../../public/list.json";
+
 function Freebook() {
-  return <></>;
+  const filterData = list.filter((data) => data.category === "Free");
+  console.log(filterData);
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free offered Books</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
+            corporis expedita temporibus! Sapiente praesentium iste facere natus
+            sed quo alias rerum vel deserunt. Doloribus porro itaque nesciunt
+            dolor commodi temporibus.
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Freebook;
