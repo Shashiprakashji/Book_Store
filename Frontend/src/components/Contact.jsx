54cf0c91162dca28358f89ee5eaf7c94f978373e
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-05-15 15:03:59 +0530

    commit

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -1,7 +1,146 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import Navbar from "../components/Navbar";
 
-function Contact() {
-  return <div>Contact</div>;
+export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <Navbar />
+      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 dark:bg-slate-900">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            Contact Us 😁
+          </h2>
+          {/* <p className="mt-2 text-lg leading-8 text-gray-600 dark:text-gray-300"></p> */}
+        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mx-auto mt-16 max-w-xl sm:mt-20"
+        >
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            {/* First Name */}
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+              >
+                First name
+              </label>
+              <input
+                type="text"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                {...register("firstName", {
+                  required: "First name is required",
+                })}
+              />
+              {errors.firstName && (
+                <span className="text-red-500">{errors.firstName.message}</span>
+              )}
+            </div>
+            {/* Last Name */}
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+              >
+                Last name
+              </label>
+              <input
+                type="text"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                {...register("lastName", { required: "Last name is required" })}
+              />
+              {errors.lastName && (
+                <span className="text-red-500">{errors.lastName.message}</span>
+              )}
+            </div>
+            {/* Email */}
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                autoComplete="email"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^\S+@\S+\.\S+$/,
+                    message: "Entered value does not match email format",
+                  },
+                })}
+              />
+              {errors.email && (
+                <span className="text-red-500">{errors.email.message}</span>
+              )}
+            </div>
+            {/* Phone Number */}
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+              >
+                Phone number
+              </label>
+              <input
+                type="tel"
+                id="phone-number"
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                {...register("phoneNumber", {
+                  required: "Phone number is required",
+                })}
+              />
+              {errors.phoneNumber && (
+                <span className="text-red-500">
+                  {errors.phoneNumber.message}
+                </span>
+              )}
+            </div>
+            {/* Message */}
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                rows={4}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                {...register("message", { required: "Message is required" })}
+              />
+              {errors.message && (
+                <span className="text-red-500">{errors.message.message}</span>
+              )}
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="mt-10 block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:ring-indigo-600"
+          >
+            Let's Talk
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
-
-export default Contact;
