29dd6aa31ed28c6c10b14a51684bfddc6439f868
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-05-15 13:46:35 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./home/Home";
 import { Routes, Route } from "react-router-dom";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/course" element={<Courses />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
         <a href="/course">Course</a>
       </li>
       <li>
-        <a>Contact</a>
+        <a href="/contact">Contact</a>
       </li>
       <li>
         <a>About</a>
