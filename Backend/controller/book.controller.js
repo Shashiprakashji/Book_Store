579e95d18b9e4b910f1046eccb1174a87595d888
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-05-19 17:22:38 +0530

    commit

--- Backend/controller/book.controller.js
+++ Backend/controller/book.controller.js
@@ -3,7 +3,7 @@ import Book from "../model/book.model.js";
 export const getBook = async (req, res) => {
   try {
     const book = await Book.find();
-    res.status(200).json(Book);
+    res.status(200).json(book);
   } catch (error) {
     console.log("Error:", error);
     res.status(500).json(error);
--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import bookRoute from "./route/book.route.js";
 const app = express();
 
 dotenv.config();
+console.log(process.env.MongoDBURI);
 
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
